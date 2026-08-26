07a70ab7148bd5d84dc32a858b5e8eddf01245dc
Arjunaditya Das <adityaarjun434@gmail.com>
2026-08-26 23:53:26 +0530

    configuration & global inputs

--- encoder_codec.py
+++ encoder_codec.py
@@ -1,3 +1,25 @@
 import json
 import hashlib
 from collections import Counter
+
+# ============================================================
+# CONFIGURATION
+# ============================================================
+
+INPUT_FILE = "binary_output.txt"
+
+DNA_OUTPUT_FILE = "final_dna_sequence.txt"
+MAPPING_OUTPUT_FILE = "final_mapping.json"
+
+# Constraint parameters
+MIN_GC = 0.40
+MAX_GC = 0.60
+
+MAX_HOMOPOLYMER = 3
+
+# k-mer constraint
+KMER_SIZE = 4
+MAX_KMER_FREQUENCY = 3
+
+# Maximum number of repeated consecutive patterns
+MAX_REPEAT_COUNT = 2
